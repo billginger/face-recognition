ab9d13eef4138f3b24c0475c2d76ed72968976d3
Bill <jfg@163.com>
2019-05-29 13:27:21 +0800

    avatar can select file

--- react-avatar/src/Avatar.jsx
+++ react-avatar/src/Avatar.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import Icon from '../../react-icon/dist/bundle.js';
 import './less/style.less';
 
-const Avatar = () => (
-	<div id="react-avatar">
-		<Icon className="icon" type="add_a_photo" color="#fff" />
-	</div>
-);
+class Avatar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { imgData: '' };
+	}
+	render() {
+		const { imgData } = this.state;
+		const iconTag = (
+			!imgData && <Icon type="add_a_photo" color="#fff" />
+		);
+		const imgTag = (
+			imgData && <img src={imgData} />
+		);
+		// Handle
+		const selectFile = () => {
+			this.refs.fileInput.click();
+		}
+		const handleFileChange = e => {
+			const files = e.target.files;
+			if (!files.length || files[0].type.indexOf('image/') != 0) {
+				return console.log('File Error!');
+			}
+			const reader = new FileReader();
+			reader.onload = () => {
+				this.setState({ imgData: reader.result });
+			}
+			reader.readAsDataURL(files[0]);
+		}
+		return (
+			<div id="react-avatar" onClick={selectFile}>
+				{iconTag}
+				<input ref="fileInput" type="file" accept="image/*" onChange={handleFileChange} />
+				{imgTag}
+			</div>
+		);
+	}
+}
 
 export default Avatar;
